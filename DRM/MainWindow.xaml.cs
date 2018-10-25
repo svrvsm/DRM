aa817a926b8602e344250c1e12238f4700be7639
VRamani <uberhexx@gmail.com>
2018-10-25 09:07:59 -0700

    Minor clean up and rebuild.

--- DRM/MainWindow.xaml.cs
+++ DRM/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
 using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
-//using System.Windows.Input;
-//using System.Windows.Media;
 using System.Windows.Media.Imaging;
-//using System.Windows.Navigation;
-//using System.Windows.Shapes;
-//using System.Windows.Forms;
 using System.Windows.Interop;
 
 namespace DRM
@@ -22,22 +11,20 @@ namespace DRM
     public partial class MainWindow : Window
     {
         const string FILTER = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
-        bool dwmEnableAttempted_ = false;
         
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            IntPtr mainHandle = new WindowInteropHelper(this).Handle;
+            DWMWrapper.EnableDWM(mainHandle);
+        }
+
         private void OnOpen(object sender, RoutedEventArgs e)
         {
-            if (!dwmEnableAttempted_)
-            {
-                IntPtr mainHandle = new WindowInteropHelper(this).Handle;
-                DWMWrapper.EnableDWM(mainHandle);
-                dwmEnableAttempted_ = true;
-            }
-            
             System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
             openFileDialog.Filter = FILTER; 
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
